5dc5909bd87e5f27e880c7ecb644bf6ddf481bf7
maoo <maoo@apache.org>
2022-07-18 14:30:13 +0200

    add nosemgrep ignores

--- maven/src/main/java/hello/HelloWorld.java
+++ maven/src/main/java/hello/HelloWorld.java
@@ -5,9 +5,11 @@ import org.joda.time.LocalTime;
 public class HelloWorld {
     public static void main(String[] args) {
       LocalTime currentTime = new LocalTime();
-		  System.out.println("The current local time is: " + currentTime);
+        // nosemgrep
+		System.out.println("The current local time is: " + currentTime);
 
         Greeter greeter = new Greeter();
+        // nosemgrep
         System.out.println(greeter.sayHello());
     }
 }
--- node/index.js
+++ node/index.js
@@ -1,6 +1,7 @@
 const http = require('http');
 const hostname = '127.0.0.1';
 const port = 3000;
+// nosemgrep
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
